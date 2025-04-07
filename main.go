3a76451d0f0ad80a356418988736f66e2374d3c5
gomarcd <git@gomarcd.dev>
2025-04-07 16:43:44 -0600

    add missing embed

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"embed"
 	"log"
-
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
@@ -11,6 +10,7 @@ import (
 	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
 
+//go:embed all:frontend/dist
 var assets embed.FS
 
 func main() {
