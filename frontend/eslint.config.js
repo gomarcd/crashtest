b1116ecb15fff02724060fe9bd5ad0b702dbbec2
gomarcd <git@gomarcd.dev>
2025-04-06 19:25:45 -0600

    add eslint config

---
+++ frontend/eslint.config.js
@@ -0,0 +1,22 @@
+import vue from 'eslint-plugin-vue';
+import typescript from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+
+export default [
+  ...vue.configs['flat/essential'],
+  ...vue.configs['flat/vue3-recommended'],
+  {
+    files: ['**/*.vue', '**/*.ts', '**/*.js'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 'latest'
+      }
+    },
+    rules: {
+      // Customize rules as needed
+      'vue/multi-word-component-names': 'off'
+    }
+  }
+];
